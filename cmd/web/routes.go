44347881603d48c276db178bdae1284ada3d7239
Neni <felipe.dacs@gmail.com>
2023-11-05 17:23:06 -0300

    :new: adiciona ws cansado

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -1,25 +1,41 @@
 package main
 
 import (
+	"log"
 	"net/http"
 
+	"github.com/go-chi/chi/middleware"
 	"github.com/go-chi/chi/v5"
-	"github.com/go-chi/chi/v5/middleware"
+	"github.com/olahol/melody"
 )
 
 func (app *application) routes() http.Handler {
 	r := chi.NewRouter()
 
-	r.Use(middleware.Recoverer)
-	r.Use(app.addIPToContext)
-	r.Use(app.Session.LoadAndSave)
+	m := melody.New()
+	r.Get("/ws", func(w http.ResponseWriter, r *http.Request) {
+		err := m.HandleRequest(w, r)
+		if err != nil {
+			log.Fatal(err)
+		}
+	})
 
-	r.Get("/", app.Home)
-	r.Post("/", app.Login)
+	m.HandleMessage(func(s *melody.Session, msg []byte) {
+		m.Broadcast(msg)
+	})
 
 	r.Group(func(r chi.Router) {
-		r.Use(app.auth)
-		r.Get("/app", app.Dashboard)
+		r.Use(middleware.Recoverer)
+		r.Use(app.addIPToContext)
+		r.Use(app.Session.LoadAndSave)
+
+		r.Get("/", app.Home)
+		r.Post("/", app.Login)
+
+		r.Group(func(r chi.Router) {
+			r.Use(app.auth)
+			r.Get("/app", app.Dashboard)
+		})
 	})
 
 	fileServer := http.FileServer(http.Dir("./static"))
--- go.mod
+++ go.mod
@@ -4,7 +4,9 @@ go 1.21.1
 
 require (
 	github.com/alexedwards/scs/v2 v2.6.0 // indirect
+	github.com/go-chi/chi v1.5.5 // indirect
 	github.com/go-chi/chi/v5 v5.0.10 // indirect
 	github.com/gorilla/websocket v1.5.1 // indirect
+	github.com/olahol/melody v1.1.4 // indirect
 	golang.org/x/net v0.17.0 // indirect
 )
